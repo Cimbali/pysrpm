b2cf0345e2d47fd9f5bf969d74d1d7ecaa5d3431
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 00:34:41 +0000

    Adding tarfile member sanitization to extractall()

--- pysrpm/rpm.py
+++ pysrpm/rpm.py
@@ -105,7 +105,26 @@ class RPM:
             tempdir = self.tempdir.resolve()
             safe_members = [info for info, dest in ((info, (tempdir / info.name).resolve()) for info in tf.getmembers())
                             if dest.parts[:len(tempdir.parts)] == tempdir.parts and not dest.exists()]
-            tf.extractall(self.tempdir, safe_members)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, self.tempdir, safe_members)
 
 
     def _make_directories(self):
